1a03d3c298f5d143feb775433da8c1ed39cbffa0
Kyle Riedemann <contact@kyleriedemann.com>
2015-07-08 20:27:32 -0500

    Initial commit

---
+++ FizzbuzzCalculator/Library1.fs
@@ -0,0 +1,8 @@
+﻿namespace FizzbuzzCalculator
+
+type FizzbuzzCalculator() = 
+    static member fizzbuzzCalc(x) : string =
+        if x % 3 = 0 && x % 5 = 0 then  "FizzBuzz"  
+        elif x % 3 = 0 then  "Fizz"
+        elif x % 5 = 0 then  "Buzz"
+        else x.ToString()
---
+++ WPFTest/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FizzbuzzCalculator;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            int x;
+            if(Int32.TryParse(InputTextBox.Text, out x))
+            {
+                OutputLabel.Content = FizzbuzzCalculator.FizzbuzzCalculator.fizzbuzzCalc(x);
+            }
+            else
+            {
+                OutputLabel.Content = "Enter an Integer";
+            }
+        }
+
+        private void InputTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.Text = null;
+        }
+    }
+}
