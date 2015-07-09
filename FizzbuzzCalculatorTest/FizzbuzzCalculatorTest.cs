a6d9e27e3a5b5911cfc39bcfa1e683d5b8a2cbce
Kyle Riedemann <contact@kyleriedemann.com>
2015-07-08 20:41:08 -0500

    Added unit tests
    Tests are written in NUnit. To run them from Visual Studio, the NUnite
    Test Runner extension needs to be installed.

---
+++ FizzbuzzCalculatorTest/FizzbuzzCalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzbuzzCalculatorTest
+{
+    [TestFixture]
+    public class FizzBuzzCalculatorTest
+    {
+        [Test]
+        public void FizzbuzzCalcReturnsFizzForThree()
+        {
+            Assert.AreEqual("Fizz", FizzbuzzCalculator.FizzbuzzCalculator.fizzbuzzCalc(3));
+        }
+
+        [Test]
+        public void FizzbuzzCalcReturnsBuzzForFive()
+        {
+            Assert.AreEqual("Buzz", FizzbuzzCalculator.FizzbuzzCalculator.fizzbuzzCalc(5));
+        }
+
+        [Test]
+        public void FizzbuzzCalcReturnsFizzBuzzForFifteen()
+        {
+            Assert.AreEqual("FizzBuzz", FizzbuzzCalculator.FizzbuzzCalculator.fizzbuzzCalc(15));
+        }
+
+        [Test]
+        public void FizzbuzzCalcReturnsNumberToStringForDefaultCase()
+        {
+            Assert.AreEqual(29.ToString(), FizzbuzzCalculator.FizzbuzzCalculator.fizzbuzzCalc(29));
+        }
+    }
+}
